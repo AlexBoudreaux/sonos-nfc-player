e1f97a3c1e52c95de7fb6dbb45ab52fc0494ead9
alex-boudreaux <43324872+AlexBoudreaux@users.noreply.github.com>
2023-10-19 22:06:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from src.nfc_reader import read_nfc_tag
 from src.sonos_controller import play_on_sonos
 from src.supabase import fetch_spotify_id
 from dotenv import load_dotenv
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 
 def main():
     load_dotenv('.env.local')
 
+    reader = SimpleMFRC522()
+
     while True:
-        tag = read_nfc_tag()
-        if tag:
-            tag_id = tag.identifier
-            spotify_media = fetch_spotify_id(tag_id)
-            if spotify_media:
-                play_on_sonos(spotify_media)
+        tag_id = reader.read()[0]
+        spotify_media = fetch_spotify_id(tag_id)
+        print(spotify_media)
+        # if spotify_media:
+        #     play_on_sonos(spotify_media)
 
 if __name__ == "__main__":
     main()
