15e0f44cf0c7f7dffa02612cc276a903312b74a8
alex-boudreaux <43324872+AlexBoudreaux@users.noreply.github.com>
2023-10-22 20:01:04 -0500

    Query the db once on start up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # from src.nfc_reader import read_nfc_tag
 from src.sonos_controller import play_on_sonos
-from src.supabase import fetch_spotify_id
+from src.supabase import fetch_spotify_id, fetch_all_media
 from dotenv import load_dotenv
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
@@ -10,10 +10,12 @@ def main():
 
     reader = SimpleMFRC522()
 
+    all_media = fetch_all_media()
+
     while True:
         print("Ready to read")
         tag_id = reader.read()[0]
-        spotify_media = fetch_spotify_id(tag_id)
+        spotify_media = fetch_spotify_id(tag_id, all_media)
         print(spotify_media)
         if spotify_media:
             play_on_sonos(spotify_media)
--- src/supabase.py
+++ src/supabase.py
@@ -7,14 +7,33 @@ def init_supabase():
     supabase = create_client(supabase_url, supabase_key)
     return supabase
 
-def fetch_spotify_id(nfc_id):
-    db = init_supabase()
-    tables = ['artists', 'albums', 'playlists']
-    media_types = ['artist', 'album', 'playlist']
+def fetch_all_media():
+    '''Fetch all songs from the database'''
 
-    for table, media_type in zip(tables, media_types):
-        result = db.table(table).select('spotify_id').eq('nfc_id', str(nfc_id)).execute()
-        if result and len(result['data'])>0:
-            return {"spotify_id": result['data'][0].get('spotify_id'), "media_type": media_type}
+    db = init_supabase()
+    data = []
+    for table in ['artists', 'albums', 'playlists']:
+        results = db.table(table).select('*').execute()
+        if results and len(results['data'])>0:
+            for item in results['data']:
+                item['media_type'] = table[:-1]
+                data.append(item)
+    return data
+
+def fetch_spotify_id(nfc_id, all_media):
+    '''Fetch the spotify_id from all media that matches the nfc_id'''
+    
+    # db = init_supabase()
+    # tables = ['artists', 'albums', 'playlists']
+    # media_types = ['artist', 'album', 'playlist']
+
+    # for table, media_type in zip(tables, media_types):
+    #     result = db.table(table).select('spotify_id').eq('nfc_id', str(nfc_id)).execute()
+    #     if result and len(result['data'])>0:
+    #         return {"spotify_id": result['data'][0].get('spotify_id'), "media_type": media_type}
+
+    for media in all_media:
+        if media.get('nfc_id') == str(nfc_id):
+            return {"spotify_id": media.get('spotify_id'), "media_type": media.get('media_type')}
 
     return None
